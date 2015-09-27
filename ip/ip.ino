5b0b6badae5cdb0f0df193fd8536b8886f33b7e3
Matheus Portela <matheus.v.portela@gmail.com>
2015-09-27 16:02:57 -0300

    Sending UDP packets.

--- ip/ip.ino
+++ ip/ip.ino
@@ -1,31 +1,61 @@
 #include <Ethernet.h>
 #include <EthernetUdp.h>
 
-EthernetUDP udp;
-unsigned int udpPort = 8888;
+#define MESSAGE_MAX_SIZE 1024
 
-char packetBuffer[UDP_TX_PACKET_MAX_SIZE];
+unsigned int udpPort = 8888;
+EthernetUDP udp;
+
+void receiveMessage(char* dest, int maxSize) {
+  int packetSize = 0;
+  IPAddress remote;
+  
+  // Clear message buffer
+  memset(dest, 0, maxSize);
+
+  // Check whether received a message
+  while (!packetSize)
+    packetSize = udp.parsePacket();
+  
+  // Read message
+  udp.read(dest, maxSize);
+  
+  // Output message to serial
+  Serial.print("Received packet of size ");
+  Serial.println(packetSize);
+  Serial.print("From ");
+  Serial.print(udp.remoteIP());
+  Serial.print(":");
+  Serial.println(udp.remotePort());
+  Serial.print("Contents: ");
+  Serial.println(dest);
+}
+
+void sendMessage(IPAddress ip, char* msg) {
+  udp.beginPacket(ip, udpPort);
+  udp.print(msg);
+  udp.endPacket();
+  
+  Serial.println("Sent message");
+  Serial.print("To: ");
+  Serial.println(ip);
+  Serial.print("Contents: ");
+  Serial.println(msg);
+}
 
 void setup() {
   system("ifconfig eth0 192.168.0.200 netmask 255.255.255.0 up");
   udp.begin(udpPort);
-  
   Serial.begin(9600);
   Serial.println(Ethernet.localIP());
 }
 
 void loop() {
-  int packetSize;
-  IPAddress remote;
+  IPAddress remoteIp(192, 168, 0, 12);
+  char message[MESSAGE_MAX_SIZE];
   
-  packetSize = udp.parsePacket();
-  if (packetSize) {
-    Serial.println("Received packet");
-    Serial.print("Size: ");
-    Serial.println(packetSize);
-    Serial.print("From: ");
-    Serial.print(udp.remoteIP());
-    Serial.print(":");
-    Serial.println(udp.remotePort());
-  }
+  receiveMessage(message, MESSAGE_MAX_SIZE);
+  sendMessage(remoteIp, message);
+
+  delay(1000);
 }
